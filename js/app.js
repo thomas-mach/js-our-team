870c4ee11f93753b8f4165e8425f42472da78a43
Tomasz Mach <m4chtomasz@gmail.com>
2024-03-18 16:43:45 +0100

    Add: Bonus 1

--- js/app.js
+++ js/app.js
@@ -11,37 +11,37 @@ function getMember(nome, ruolo, foto){
 const member1 = getMember(
     'Wane Barnett', 
     'Founder & COE', 
-    'wayne-barnett-founder-ceo.jpg'
+    'img/wayne-barnett-founder-ceo.jpg'
 )
  
 const member2 = getMember(
     'Angela Caroll', 
     'Chief Editor',	        
-    'angela-caroll-chief-editor.jpg'
+    'img/angela-caroll-chief-editor.jpg'
 )
 
 const member3 = getMember(
     'Walter Gordon',
     'Office Manager',	        
-    'walter-gordon-office-manager.jpg'
+    'img/walter-gordon-office-manager.jpg'
 )
 
 const member4 = getMember(
     'Angela Lopez',
     'Social Media Manager',
-    'angela-lopez-social-media-manager.jpg'
+    'img/angela-lopez-social-media-manager.jpg'
 )
 
 const member5 = getMember(
     'Scott Estrada', 
     'Developer',	            
-    'scott-estrada-developer.jpg'
+    'img/scott-estrada-developer.jpg'
 )
 
 const member6 = getMember(
     'Barbara Ramos',	
     'Graphic Designer',	   
-    'barbara-ramos-graphic-designer.jpg'
+    'img/barbara-ramos-graphic-designer.jpg'
 )
 // creare arrey degli oggetti
 const members = [member1, member2, member3, member4, member5, member6]
@@ -51,13 +51,20 @@ const members = [member1, member2, member3, member4, member5, member6]
 for (let i = 0; i < members.length; i++){
     let newMember = document.createElement('div')// cera nuovo div nel body per ogni elemento del array members 
     document.body.appendChild(newMember) //aggiunge div per ogni elemento
+    let newIMG = document.createElement('img')
+    console.log(members[i].foto)
+    newIMG.src = src= members[i].foto
+    newMember.appendChild(newIMG)
     // creare ciclo (for in) per scorrere oggetto    
     for(let key in members[i]){
         // stampare valore di ogni chiave
-        console.log(key +': '+ members[i][key])
+        // console.log(key +': '+ members[i][key])
     let newParagraf = document.createElement('p') //crea un paragrafo per ogni chiave
     newParagraf.textContent = `${key}: ${members[i][key]}` //aggiunge chiave  e valore
     newMember.appendChild(newParagraf) // appende paragrafo a div creato nel ciclo for
+    
+
     }
 }
+
 
