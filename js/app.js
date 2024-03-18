43d5c9b894a7ae8fc4312de9fdf6ffa17fd7d4a1
Tomasz Mach <m4chtomasz@gmail.com>
2024-03-18 19:40:27 +0100

    Add: refactory Bonus2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-console.log('js')
-//Dichiarare la funzione per creare oggetti 
 function getMember(nome, ruolo, foto){
     return{
         nome: nome,
@@ -7,7 +5,7 @@ function getMember(nome, ruolo, foto){
         foto: foto
     }
 }
-// creare gli oggetti evocando la funzione
+
 const member1 = getMember(
     'Wane Barnett', 
     'Founder & COE', 
@@ -43,40 +41,21 @@ const member6 = getMember(
     'Graphic Designer',	   
     'img/barbara-ramos-graphic-designer.jpg'
 )
-// creare arrey degli oggetti
 const members = [member1, member2, member3, member4, member5, member6]
 const cardsEl = document.getElementById('cards')
 
-// creare ciclo (for) per scorrere array members
-
 for (let i = 0; i < members.length; i++){
-    
-    let newMember = document.createElement('div')
-    newMember.classList.add('col-4')
-    cardsEl.appendChild(newMember) 
-
-    let newIMG = document.createElement('img')
-    newIMG.src = members[i].foto
-    newMember.appendChild(newIMG)
-
-    let newParagraf = document.createElement('p')
-    newParagraf.textContent = members[i].nome
-    newMember.appendChild(newParagraf)
-
-    let ruoloParagraf = document.createElement('p')
-    ruoloParagraf.textContent = members[i].ruolo
-    newMember.appendChild(ruoloParagraf)
-
-    // creare ciclo (for in) per scorrere oggetto    
-    // for(let key in members[i]){
-    //     // stampare valore di ogni chiave
-    //     // console.log(key +': '+ members[i][key])
-    // let newParagraf = document.createElement('p') //crea un paragrafo per ogni chiave
-    // newParagraf.textContent = `${key}: ${members[i][key]}` //aggiunge chiave  e valore
-    // newMember.appendChild(newParagraf) // appende paragrafo a div creato nel ciclo for
-    
-
-    // }
+    cardsEl.innerHTML += `    
+        <div class="col">
+            <div class="card border-0 rounded-0">
+             <img src=${members[i].foto} class="card-img-top rounded-0 ">
+                <div class="card-body">
+                    <h5 class="card-title text-center">${members[i].nome}</h5>
+                    <p class="card-text text-center">${members[i].ruolo}</p>
+                </div>
+            </div>
+        </div>
+    `
 }
 
 
